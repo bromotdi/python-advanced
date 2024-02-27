ff0fd106ad270f78f3d48b023841e2b26728b004
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-27 22:31:46 +0200

    Update challenges.py

--- program-flow-control/challenges.py
+++ program-flow-control/challenges.py
@@ -114,7 +114,7 @@ print("*"*80)
 # use else in a loop - actually has another purpose
 # v contrived example
 
-numbers= [1,45,32,12,60]
+numbers = [1,45,32,12,60]
 # Here the else only fires on a successful loop through all elements, if break happens at all we wont see the else
 #  clause
 for num in numbers:
@@ -126,87 +126,86 @@ else:
     print('These numbers are ok')
 
 
+# Using this version of else in high low game.
+# Will only work with some numbers where convergence is possible
+## ie 72 but not 73
 
-# Using this version of else in high low game. Will only work with some numbers where convergence is possible, ie 72
-# but not 73
+low = 1
+high = 1000
+print('Please think of a number between {} and {}'.format(low,high))
+input('Press enter to start: ')
+guesses = 0
+while low != high:
+    print('\tGuessing in the range of {} to {}'.format(low,high))
+    guess = low +(high-low) //2
+    high_low = input('My guess is {}. '
+                     'Should I go higher or lower or am I correct '.format(guess)).casefold()
+    guesses += 1
+    # This is how pass would be used if we wrote the actual logic later on
+    # if high_low == 'higher':
+    #     # Guess higher - the low end of range becomes 1 greater than guess
+    #     pass
+    if high_low == 'higher':
+        # Guess higher - the low end of range becomes 1 greater than guess
+        low = guess + 1
+    elif high_low =='lower':
+        # Guess lower - the high end of the range becomes 1 less than guess
+        high = guess - 1
+    elif high_low == 'correct':
+        print('You got it! ')
+        print('I got it in {} guesses'.format(guesses))
+        break
+    else:
+        print('Choices are only higher,lower or correct')
+else:
+    print('You thought of the number {}'.format(low))
+    print('I got it in {} guesses'.format(guesses))
 
-# low = 1
-# high = 1000
-# print('Please think of a number between {} and {}'.format(low,high))
-# input('Press enter to start: ')
-# guesses = 0
-# while low != high:
-#     #print('\tGuessing in the range of {} to {}'.format(low,high))
-#     guess = low +(high-low) //2
-#     high_low = input('My guess is {}. '
-#                      'Should I go higher or lower or am I correct '.format(guess)).casefold()
-#     guesses += 1
-#     # This is how pass would be used if we wrote the actual logic later on
-#     # if high_low == 'higher':
-#     #     # Guess higher - the low end of range becomes 1 greater than guess
-#     #     pass
-#     if high_low == 'higher':
-#         # Guess higher - the low end of range becomes 1 greater than guess
-#         low = guess + 1
-#     elif high_low =='lower':
-#         # Guess lower - the high end of the range becomes 1 less than guess
-#         high = guess - 1
-#     elif high_low == 'correct':
-#         print('You got it! ')
-#         print('I got it in {} guesses'.format(guesses))
-#         break
-#     else:
-#         print('Choices are only higher,lower or correct')
-# else:
-#     print('You thought of the number {}'.format(low))
-#     print('I got it in {} guesses'.format(guesses))
+## Use else with the loop the same way you do with for loops
 
-# Use else with the loop the same way you do with for loops
-# available_exits = ['west','east','south','north']
-# chosen_exit = " "
-# while chosen_exit not in available_exits:
-#     chosen_exit = input("Please choose a direction: ")
-#     if chosen_exit.casefold() =='quit':
-#         print('You gave up')
-#         break
-# else:
-#     print("You escaped")
+available_exits = ['west','east','south','north']
+chosen_exit = " "
+while chosen_exit not in available_exits:
+    chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() =='quit':
+        print('You gave up')
+        break
+else:
+    print("You escaped")
 
 
-# print('Hello my name\t is Eoin. What are\n you doing\n today')
+print('Hello my name\t is Eoin. What are\n you doing\n today')
 
 # Ending challenges
-#print('-'*80)
-#options = {1:'Hello ',2:'Fuck off', 3:'20:10', 4:'-.-',5:'quit'}
-# while True:
-#     print('[1] Say hello\n[2] Be rude\n[3] Display time\n[4] Smile\n[5] Quit')
-#     choice = options[int(input('Choose an option: '))]
-#     if choice=='quit':
-#         print('Exiting...')
-#         break
-#     else:
-#         print(choice)
+print('-'*80)
+options = {1:'Hello ',2:'Fuck off', 3:'20:10', 4:'-.-',5:'quit'}
+while True:
+    print('[1] Say hello\n[2] Be rude\n[3] Display time\n[4] Smile\n[5] Quit')
+    choice = options[int(input('Choose an option: '))]
+    if choice=='quit':
+        print('Exiting...')
+        break
+    else:
+        print(choice)
 
-
-# My attempt at it
-# choice = None
-# valid_options = [0,1,2,3,4,5]
-# while choice != 0:
-#     options_msg = '[1] Learn Python\n[2] learn Java\n[3] ' \
-#                   'Learn KDB\n[4] ' \
-#                   'Have Dinner\n[5] Go to Bed\n[0] Exit\n Please choose your options from the list above: '
-#     choice = int(input(options_msg))
-#     while not choice in valid_options:
-#         print('Invalid choice, please choose again')
-#         # Could argue this is not necessary, diff between loop til you pick a valid option vs just printing the
-#         # message again. Think behaviour resolves to same so maybe secondary loop not necessary and could do
-#         # as below ?
-#         choice = int(input(options_msg))
-#     if choice>0:
-#         print('You chose option {}'.format(choice))
-#     else:
-#         print('Exiting...')
-#         break
+choice = None
+valid_options = [0,1,2,3,4,5]
+while choice != 0:
+    options_msg = '[1] Learn Python\n[2] learn Java\n[3] ' \
+                  'Learn KDB\n[4] ' \
+                  'Have Dinner\n[5] Go to Bed\n[0] Exit\n Please choose your options from the list above: '
+    choice = int(input(options_msg))
+    while not choice in valid_options:
+        print('Invalid choice, please choose again')
+        # Could argue this is not necessary, diff between loop til you pick a valid option vs just printing the
+        # message again. Think behaviour resolves to same so maybe secondary loop not necessary and could do
+        # as below ?
+        choice = int(input(options_msg))
+    if choice>0:
+        print('You chose option {}'.format(choice))
+    else:
+        print('Exiting...')
+        break
 
 
 # His go at it
