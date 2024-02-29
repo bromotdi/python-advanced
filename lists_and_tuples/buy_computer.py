84ea8a1b048a4229224b29bdbc2da39be495c9fc
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-29 21:37:51 +0200

    Update buy_computer.py

--- lists_and_tuples/buy_computer.py
+++ lists_and_tuples/buy_computer.py
@@ -1,46 +1,69 @@
-# current_choice= "0"
-# computer_parts = [] # create empty list
-# parts_dict = {'1': 'computer','2': 'monitor','3': 'keyboard','4': 'mouse','5': 'mouse mat','6':' HDMI cable'}
-# while current_choice != '0':
-#     if current_choice in "123456":
-#         if current_choice not in computer_parts:
-#             print('Adding {} to shopping list'.format(parts_dict[current_choice]))
-#             computer_parts.append(parts_dict[current_choice])
-#     else:
-#         print('Please add options from the list below: ')
-#         print("1: computer\n2: monitor\n3: keyboard\n4: mouse\n5: mouse mat\n6: HDMI cable\n0: to finish ")
-#     current_choice = input()
-# else:
-#     print('Final list {}'.format(computer_parts))
-#
-#
-# # Iterate over the list
+current_choice= "0"
+computer_parts = [] # create empty list
+parts_dict = {'1': 'computer','2': 'monitor','3': 'keyboard','4': 'mouse','5': 'mouse mat','6':' HDMI cable'}
+while current_choice != '0':
+    if current_choice in "123456":
+        if current_choice not in computer_parts:
+            print('Adding {} to shopping list'.format(parts_dict[current_choice]))
+            computer_parts.append(parts_dict[current_choice])
+    else:
+        print('Please add options from the list below: ')
+        print("1: computer\n2: monitor\n3: keyboard\n4: mouse\n5: mouse mat\n6: HDMI cable\n0: to finish ")
+    current_choice = input()
+else:
+    print('Final list {}'.format(computer_parts))
+
+
+# Iterate over the list
+available_parts = ['computer','monitor','keyboard','mouse','mouse mat','HDMI cable']
+current_choice= "0"
+computer_parts = [] # create empty list
+while current_choice != '0':
+    if current_choice in "123456":
+        if current_choice not in computer_parts:
+            print('Adding {} to shopping list'.format(parts_dict[current_choice]))
+            computer_parts.append(parts_dict[current_choice])
+    else:
+        print('Please add options from the list below: ')
+        for part in available_parts:
+            print("{0}:{1}".format(available_parts.index(part)+1,part))
+    current_choice = input()
+else:
+    print('Final list {}'.format(computer_parts))
+
+
+# Iterate over the list - use enumerate
+available_parts = ['computer','monitor','keyboard','mouse','mouse mat','HDMI cable']
+current_choice= "0"
+computer_parts = [] # create empty list
+while current_choice != '0':
+    if current_choice in "123456":
+        if current_choice not in computer_parts:
+            print('Adding {} to shopping list'.format(available_parts[-1+int(current_choice)]))
+            computer_parts.append(available_parts[-1+int(current_choice)])
+    else:
+        print('Please add options from the list below: ')
+        for i,part in enumerate(available_parts):
+            print("{0}:{1}".format(i+1,part))
+    current_choice = input()
+else:
+    print('Final list {}'.format(computer_parts))
+
+
+
+# Same again but would list cmprehension
+print('*'*80)
 # available_parts = ['computer','monitor','keyboard','mouse','mouse mat','HDMI cable']
-# current_choice= "0"
+# valid_choice =[str(i) for i in range(0,1 + len(available_parts))]
+# print(valid_choice)
+# current_choice= "-"
 # computer_parts = [] # create empty list
 # while current_choice != '0':
-#     if current_choice in "123456":
-#         if current_choice not in computer_parts:
-#             print('Adding {} to shopping list'.format(parts_dict[current_choice]))
-#             computer_parts.append(parts_dict[current_choice])
-#     else:
-#         print('Please add options from the list below: ')
-#         for part in available_parts:
-#             print("{0}:{1}".format(available_parts.index(part)+1,part))
-#     current_choice = input()
-# else:
-#     print('Final list {}'.format(computer_parts))
-#
-#
-# # Iterate over the list - use enumerate
-# available_parts = ['computer','monitor','keyboard','mouse','mouse mat','HDMI cable']
-# current_choice= "0"
-# computer_parts = [] # create empty list
-# while current_choice != '0':
-#     if current_choice in "123456":
-#         if current_choice not in computer_parts:
-#             print('Adding {} to shopping list'.format(available_parts[-1+int(current_choice)]))
-#             computer_parts.append(available_parts[-1+int(current_choice)])
+#     if current_choice in valid_choice:
+#         item = available_parts[-1+int(current_choice)]
+#         if item not in computer_parts:
+#             print('Adding {} to shopping list'.format(item))
+#             computer_parts.append(item)
 #     else:
 #         print('Please add options from the list below: ')
 #         for i,part in enumerate(available_parts):
@@ -49,63 +72,40 @@
 # else:
 #     print('Final list {}'.format(computer_parts))
 #
-#
-#
-# # Same again but would list cmprehension
-# print('*'*80)
-# # available_parts = ['computer','monitor','keyboard','mouse','mouse mat','HDMI cable']
-# # valid_choice =[str(i) for i in range(0,1 + len(available_parts))]
-# # print(valid_choice)
-# # current_choice= "-"
-# # computer_parts = [] # create empty list
-# # while current_choice != '0':
-# #     if current_choice in valid_choice:
-# #         item = available_parts[-1+int(current_choice)]
-# #         if item not in computer_parts:
-# #             print('Adding {} to shopping list'.format(item))
-# #             computer_parts.append(item)
-# #     else:
-# #         print('Please add options from the list below: ')
-# #         for i,part in enumerate(available_parts):
-# #             print("{0}:{1}".format(i+1,part))
-# #     current_choice = input()
-# # else:
-# #     print('Final list {}'.format(computer_parts))
-# #
-#
-# # challenge
-# data = [
-#     "Andromeda - Shrub",
-#     "Bellflower - Flower",
-#     "China Pink - Flower",
-#     "Daffodil - Flower",
-#     "Evening Primrose - Flower",
-#     "French Marigold - Flower",
-#     "Hydrangea - Shrub",
-#     "Iris - Flower",
-#     "Japanese Camellia - Shrub",
-#     "Lavender - Shrub",
-#     "Lilac - Shrub",
-#     "Magnolia - Shrub",
-#     "Peony - Shrub",
-#     "Queen Anne's Lace - Flower",
-#     "Red Hot Poker - Flower",
-#     "Snapdragon - Flower",
-#     "Sunflower - Flower",
-#     "Tiger Lily - Flower",
-#     "Witch Hazel - Shrub",
-# ]
-# flowers = [fl for fl in data if 'Flower' in fl]
-# shrubs = [fl for fl in data if 'Flower' not in fl]
-# print(flowers)
-# print(shrubs)
-# # for item in enumerate(data):
-# #     if 'Flower' in item:
-# #         flowers += item
-# #     else""
-# #         shrubs += item
-#
-# # write your code here
+
+# challenge
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+flowers = [fl for fl in data if 'Flower' in fl]
+shrubs = [fl for fl in data if 'Flower' not in fl]
+print(flowers)
+print(shrubs)
+# for item in enumerate(data):
+#     if 'Flower' in item:
+#         flowers += item
+#     else""
+#         shrubs += item
+
+# write your code here
 
 
 ## Adding code to remove element from list
