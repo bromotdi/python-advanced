36b2117f12f7d9a73a1f98fb29896c2350947761
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-29 22:04:25 +0200

    Update nospam.py

--- lists_and_tuples/nospam.py
+++ lists_and_tuples/nospam.py
@@ -8,19 +8,20 @@ menu = [
     ['spam', 'egg', 'spam', 'spam', 'bacon', 'spam'],
     ['spam', 'sausage', 'spam', 'bacon', 'spam', 'tomato', 'spam'],
 ]
+
 # challenge to print out these lists but with spam removed
 # method one - remove spam and then print list
 # method two - print items in list as long as its not spam
 
 #one
 # Use reversed() not reverse
-# for meal in menu:
-#     top_ind = len(meal)-1
-#     for ind,item in enumerate(reversed(meal)):
-#         if item == 'spam':
-#             del meal[top_ind - ind]
-#     print(meal)
-# print(menu)
+for meal in menu:
+    top_ind = len(meal)-1
+    for ind,item in enumerate(reversed(meal)):
+        if item == 'spam':
+            del meal[top_ind - ind]
+    print(meal)
+print(menu)
 # Better way to do one
 
 for meal in menu:
