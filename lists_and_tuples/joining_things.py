a69c0695f6497f0ce871242e1c1d815a15f38a10
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-29 21:45:37 +0200

    Update joining_things.py

--- lists_and_tuples/joining_things.py
+++ lists_and_tuples/joining_things.py
@@ -8,9 +8,9 @@ flowers = ['daffodil',
 
 numbers = [1.0, 3.5, 2.1, 6.8, 10.2, 4.5]
 
-separator=' | '
+separator = ' | '
 output = separator.join(flowers)
 print(output)
+
 for flower in flowers:
     print(flower)
-
