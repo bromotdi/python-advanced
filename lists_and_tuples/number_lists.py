e3c2a306b672674e7c62a802ead61a44cb959441
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-02-29 22:09:00 +0200

    Update number_lists.py

--- lists_and_tuples/number_lists.py
+++ lists_and_tuples/number_lists.py
@@ -1,5 +1,6 @@
 even = [2,4,6,8]
 odd = [1,3,5,7,9]
+
 print(min(even))
 print(odd.max())
 print()
@@ -43,6 +44,3 @@ for ind,lst in enumerate(numbers):
     print('Printing {} list'.format(ind+1))
     for j in lst:
         print('Element {}'.format(j))
-
-
-
