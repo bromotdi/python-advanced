cc1e420ae4d279e3ebe4c6599034f1eac28ff7a1
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-01 21:44:33 +0200

    Update fibonacci.py

--- functions/fibonacci.py
+++ functions/fibonacci.py
@@ -6,18 +6,9 @@ def fibonacci(n):
     :return: List of fibonacci numbers
     """
     fib = [0,1]
-    # -2 cos we start off with first two
     for i in range(n-2):
         fib.append(fib[i] + fib[i+1])
     return fib
 
-
 result =  fibonacci(20)
 print(len(result))
-
-
-## Work was done on functions.py page
-
-
-
-
