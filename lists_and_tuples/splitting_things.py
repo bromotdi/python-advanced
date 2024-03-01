dc6bcc4ff0007920fec8d4186185e708eae50eae
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-01 20:59:15 +0200

    Update splitting_things.py

--- lists_and_tuples/splitting_things.py
+++ lists_and_tuples/splitting_things.py
@@ -1,20 +1,15 @@
 panagram = "The quick brown fox jumps over the lazy dog"
 
 words = panagram.split(", ")
-#print(words)
-
+print(words)
 print("".join(panagram))
-
-
-
 
 # Code challenge
 numbers = ['9','223','372','036','854','775','807']
 
-# for ind,num in enumerate(numbers):
-#     numbers[ind] = int(num)
-#
-# print(numbers)
+for ind,num in enumerate(numbers):
+    numbers[ind] = int(num)
+print(numbers)
 
 # his way to do it, values in place
 for ind in range(len(numbers)):
@@ -26,9 +21,6 @@ for ind in range(len(numbers)):
     new.append(int(numbers[ind]))
 print(new)
 
-
-
-
 # program to add three numbers
 numbers = input("Please enter 3 comma separated numbers: ")
 tokens=numbers.split(',')
@@ -39,9 +31,8 @@ a,b,c = tokens
 result = a+b-c
 print(result)d
 
-# other way
-# out=0
-# for val in tokens:
-#     out += int(val)
-# else:
-#     print(out)
+out=0
+for val in tokens:
+    out += int(val)
+else:
+    print(out)
