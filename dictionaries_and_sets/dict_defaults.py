87233baf1f149dbadd26ebf9e3898bb54ef69402
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-03 22:12:42 +0200

    Update dict_defaults.py

--- dictionaries_and_sets/dict_defaults.py
+++ dictionaries_and_sets/dict_defaults.py
@@ -3,14 +3,11 @@ from contents import pantry
 chicken_quantity = pantry.setdefault('chicken',0)
 print(f'chicken: {chicken_quantity}')
 
-
 beans_quantity = pantry.setdefault('beans',0)
 print(f'beans: {beans_quantity}')
 
 ketchup_quantity = pantry.get('ketchup',0)
 print(f'ketchup: {ketchup_quantity}')
-
-# exercise
 
 # We need an empty dictionary, to store and display the letter frequencies.
 word_count = {}
@@ -22,7 +19,6 @@ text = "Later in the course, you'll see how to use the collections Counter class
 for letter in text.casefold():
     if letter.isalnum():
         word_count[letter] = word_count.setdefault(letter,0) + 1
-
 
 # Printing the dictionary
 for letter, count in sorted(word_count.items()):
