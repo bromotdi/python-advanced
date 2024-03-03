5e1ae974e743b4d5cdd600f3f7365884a2a032b8
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-03 21:56:24 +0200

    Update buy_computer_dict.py

--- dictionaries_and_sets/buy_computer_dict.py
+++ dictionaries_and_sets/buy_computer_dict.py
@@ -6,23 +6,23 @@ available_parts = {
     '5':'hdmi cable',
     '6':'dvd drive',
 }
-# My way
-# current_choice = None
-# Challenge
-# while current_choice != '0':
-#     if current_choice in available_parts:
-#         chosen_part = available_parts[current_choice]
-#         print(f"Adding {chosen_part}")
-#     else:
-#         print(f'{current_choice} is an invalid choice')
-#     [print(f"[{k}]: {v}") for k,v in available_parts.items()]
-#     current_choice = input('> ')
-# else:
-#     print('Exiting...')
 
-# His way - makes sense to only printr menu when chose invalid option
 current_choice = None
+
 # Challenge
+while current_choice != '0':
+    if current_choice in available_parts:
+        chosen_part = available_parts[current_choice]
+        print(f"Adding {chosen_part}")
+    else:
+        print(f'{current_choice} is an invalid choice')
+    [print(f"[{k}]: {v}") for k,v in available_parts.items()]
+    current_choice = input('> ')
+else:
+    print('Exiting...')
+
+# makes sense to only printr menu when chose invalid option
+current_choice = None
 while current_choice != '0':
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
