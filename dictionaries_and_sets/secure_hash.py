31098d18bb8dcb5978c8b5bb9eb716eb6622892f
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-04 22:12:40 +0200

    Update secure_hash.py

--- dictionaries_and_sets/secure_hash.py
+++ dictionaries_and_sets/secure_hash.py
@@ -2,18 +2,18 @@ import hashlib
 
 # All python distributions have these
 print(sorted(hashlib.algorithms_guaranteed))
+
 # These are all available to us, but might not be in all python versions
 print(sorted(hashlib.algorithms_available))
 
-
 python_programme = """for i in range(10):
-print(i)
-"""
+                        print(i)
+                   """
 print(python_programme)
 
-#Use the encode bit to encode strings as a byte array, cant encode the string itself
-# for b in python_programme.encode('utf8'):
-#     print(b, chr(b))
+# Use the encode bit to encode strings as a byte array, cant encode the string itself
+for b in python_programme.encode('utf8'):
+    print(b, chr(b))
 
 original_hash = hashlib.sha256(python_programme.encode('utf8'))
 print(f"SHA256: {original_hash.hexdigest()}")
