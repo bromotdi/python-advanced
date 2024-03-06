4b860b545dd3189fa79d129295c37e293eeb14ba
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-06 23:03:02 +0200

    Update global_temps.py

--- reading_and_writing_files/global_temps.py
+++ reading_and_writing_files/global_temps.py
@@ -1,24 +1,17 @@
 import json
 import urllib.request
 
+json_data_source = 'temperature_anomaly.json'
 
+with open(json_data_source,encoding='utf-8') as data:
+    anomalies = json.load(data)
+print(anomalies['description'])
+print(anomalies['citation'])
 
-# json_data_source = 'temperature_anomaly.json'
-#
-#
-# with open(json_data_source,encoding='utf-8') as data:
-#     anomalies = json.load(data)
-#
-# print(anomalies['description'])
-# print(anomalies['citation'])
-#
-# for year,value in anomalies['data'].items():
-#     year,value = int(year), float(value)
-#     # this is handy to know, within f string round to two deciamls + place values 6 over so all aligned
-#     print(f"{year}...{value:6.2f}")
-# print('*'*80)
-
-
+for year,value in anomalies['data'].items():
+    year,value = int(year), float(value)
+    # this is handy to know, within f string round to two deciamls + place values 6 over so all aligned
+    print(f"{year}...{value:6.2f}")
 
 # getting the dat from url
 json_data_source = 'https://www.ncdc.noaa.gov/cag/time-series/global/globe/land_ocean/ytd/12/1880-2021/data.json'
@@ -31,12 +24,8 @@ with urllib.request.urlopen(json_data_source) as json_stream:
     # in json format
     anomalies = json.loads(data)
 
-
 for year,value in anomalies['data'].items():
     year,value = int(year), float(value)
     # this is handy to know, within f string round to two deciamls + place values 6 over so all aligned
     print(f"{year}...{value:6.2f}")
 print('*'*80)
-
-
-
