d1503b3634885fceef560c010e7d9afb6a18f464
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-07 21:59:29 +0200

    Update stripping.py

--- reading_and_writing_files/stripping.py
+++ reading_and_writing_files/stripping.py
@@ -8,18 +8,16 @@ chars = "'Twasebv"
 no_apostrophe = first.strip(chars)
 print(no_apostrophe)
 
-# Write a remove prefix & remove suffix function
+# remove prefix & remove suffix function
+char = "'Twas"
+print(first.split(char)[1])
 
-# char = "'Twas"
-# print(first.split(char)[1])
-#
-# def removeprefix(string:str, prefix:str) -> str:
-#     if string.startswith(prefix):
-#         return string[len(prefix):]
-#     else:
-#         return string[:] # remove a copy of string
-#
-#
-# def removesuffix(string,suffix):
-#     if string.endswith(suffix):
-#         return string[:-len(prefix)]
+def removeprefix(string:str, prefix:str) -> str:
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    else:
+        return string[:] # remove a copy of string
+
+def removesuffix(string,suffix):
+    if string.endswith(suffix):
+        return string[:-len(prefix)]
