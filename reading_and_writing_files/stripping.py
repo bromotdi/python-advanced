16bbb7638084710287b567d1abb8ca98bddd40c8
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-07 21:59:05 +0200

    Update stripping.py

--- reading_and_writing_files/stripping.py
+++ reading_and_writing_files/stripping.py
@@ -1,13 +1,12 @@
 filename = 'Jabberwocky.txt'
+
 with open(filename) as poem:
     first = poem.readline().rstrip()
-
 print(first)
 
 chars = "'Twasebv"
 no_apostrophe = first.strip(chars)
 print(no_apostrophe)
-
 
 # Write a remove prefix & remove suffix function
 
