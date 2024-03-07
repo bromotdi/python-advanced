0f3903dd296012c1c7bdce724293f4df94f8ce4d
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-07 22:06:35 +0200

    Update practice.py

--- reading_and_writing_files/practice.py
+++ reading_and_writing_files/practice.py
@@ -1,20 +1,14 @@
 import csv
 
 filename = 'invoices.csv'
-
 data=[]
 with open(filename, encoding='utf-8',newline='') as readfile:
     reader = csv.reader(readfile, delimiter ='\t')
     for row in reader:
         data.append(row)
-
 print(data)
+
 savefile = 'invoices2.csv'
 with open(savefile,'w',encoding='utf-8',newline='') as writefile:
     writer = csv.writer(writefile,delimiter= '\t')
     writer.writerows(data)
-
-
-
-
-
