83ca3ed793199af929b0c40c9b4ba9d4855c8870
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-10 22:54:42 +0200

    Add files via upload

---
+++ modules_and_functions/turtle_demo.py
@@ -0,0 +1,31 @@
+from turtle import forward, right,done
+import turtle
+
+
+#turtle.Screen().tracer(0) #disable turtle animation
+forward(150)
+right(240)
+forward(150)
+turtle.circle(86.5)
+forward(150)
+turtle.left(120)
+forward(150)
+turtle.circle(86.5)
+forward(150)
+turtle.left(120)
+forward(150)
+turtle.left(90)
+forward(260)
+
+done()
+
+
+
+
+
+
+
+
+
+
+
