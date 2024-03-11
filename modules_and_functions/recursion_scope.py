4148618381d9664a3cf124a1e8bcd0def39cba20
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-11 23:35:58 +0200

    Update recursion_scope.py

--- modules_and_functions/recursion_scope.py
+++ modules_and_functions/recursion_scope.py
@@ -16,23 +16,20 @@ def recursive_factorial(n):
         return 1
     else:
         return n * recursive_factorial(n-1)
-# print(recursive_factorial(1))
-# print(recursive_factorial(2))
-# print(recursive_factorial(3))
+        
+print(recursive_factorial(1))
+print(recursive_factorial(2))
+print(recursive_factorial(3))
 
 
-# fibonacci recursive
-# my attempt
-# consider the sequence as [1,1,2,3]
+# fibonacci recursive: consider the sequence as [1,1,2,3]
 def fib_recursion(n):
     if n <= 2:
         return 1
     else:
         return fib_recursion(n-1) + fib_recursion(n-2)
 
-
-# his version
-# he considers the sequence as [0, 1,1,2,3]
+# considers the sequence as [0, 1,1,2,3]
 def fib(n):
     "F(n) = F(n - 1) + F(n - 2)"
     if n < 2:
@@ -40,7 +37,7 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-# Also his way, doing it iterably
+# doing it iterably
 def fibonacci(n):
     if n==0:
         result = 0
