d6a24ec0ef7098eaa57a9d0d48239d2268132829
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-11 23:45:47 +0200

    Update timezone_challenge.py

--- modules_and_functions/timezone_challenge.py
+++ modules_and_functions/timezone_challenge.py
@@ -15,28 +15,28 @@ import datetime as dt
 import pytz
 
 timezone_dict = {str(i+1):v for i,v in enumerate(pytz.all_timezones[:9])}
-# while True:
-#     {print(f"[{k}]: v") for k,v in timezone_dict.items()}
-#     choice = input('Please choose a timezone from the list :> ')
-#     if choice in timezone_dict:
-#         chosen_tz = pytz.timezone(timezone_dict[choice])
-#         local_time = dt.datetime.now(tz=chosen_tz)
-#         utc_time = dt.datetime.utcnow()
-#
-#         print(f"Aware local time {pytz.utc.localize(local_time)}\nAware UTC {pytz.utc.localize(utc_time)}")
-#     elif choice == 0:
-#         break
-#     else:
-#         print('invalid choice - choose from the options: ')
 
-#timezone_dict = {i+1:v for i,v in enumerate(pytz.all_timezones[:9])}
+while True:
+    {print(f"[{k}]: v") for k,v in timezone_dict.items()}
+    choice = input('Please choose a timezone from the list :> ')
+    if choice in timezone_dict:
+        chosen_tz = pytz.timezone(timezone_dict[choice])
+        local_time = dt.datetime.now(tz=chosen_tz)
+        utc_time = dt.datetime.utcnow()
+        print(f"Aware local time {pytz.utc.localize(local_time)}\nAware UTC {pytz.utc.localize(utc_time)}")
+    elif choice == 0:
+        break
+    else:
+        print('invalid choice - choose from the options: ')
+
+timezone_dict = {i+1:v for i,v in enumerate(pytz.all_timezones[:9])}
 print(timezone_dict)
 {print(f"[{k}]: {v}") for k,v in timezone_dict.items()}
+
 while True:
     choice = input('Please choose a timezone from the list :> ')
     if choice =='0':
         break
-
     if choice in timezone_dict:
         tz_to_display = pytz.timezone(timezone_dict[choice])
         world_time = dt.datetime.now(tz=tz_to_display)
@@ -44,6 +44,3 @@ while True:
         print(f"Local time is {dt.datetime.now().strftime('%A %x %X')}")
         print(f"UTC time is {dt.datetime.utcnow().strftime('%A %x %X')}")
         print()
-
-
-
