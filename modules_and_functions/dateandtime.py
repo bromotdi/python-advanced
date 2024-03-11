75ef0f587009d6d405b96da8f4c340ab1d06285f
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-11 23:28:04 +0200

    Update dateandtime.py

--- modules_and_functions/dateandtime.py
+++ modules_and_functions/dateandtime.py
@@ -11,11 +11,8 @@ if time.daylight != 0:
 print(f"Local time is {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
 print(f"UTC time is {time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())}")
 
-
 # daytime is the name of the module but also a class within that
 print(35*'*' + " DATETIME " + 35*'*')
 print(datetime.datetime.today())
 print(datetime.datetime.now())
 print(datetime.datetime.utcnow())
-
-
