85a7d04c17dd5c9e25ddcdfd9ddb7b130d28c519
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-11 23:52:10 +0200

    Update turtle_demo.py

--- modules_and_functions/turtle_demo.py
+++ modules_and_functions/turtle_demo.py
@@ -1,19 +1,23 @@
 from turtle import forward, right,done
 import turtle
 
-
-#turtle.Screen().tracer(0) #disable turtle animation
+turtle.Screen().tracer(0) #disable turtle animation
 forward(150)
 right(240)
 forward(150)
+
 turtle.circle(86.5)
 forward(150)
+
 turtle.left(120)
 forward(150)
+
 turtle.circle(86.5)
 forward(150)
+
 turtle.left(120)
 forward(150)
+
 turtle.left(90)
 forward(260)
 
