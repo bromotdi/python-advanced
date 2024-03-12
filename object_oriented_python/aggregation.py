7bbbed6ebc0c28a7ed98c4178fdaee98d46e61ee
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-12 22:39:43 +0200

    Update aggregation.py

--- object_oriented_python/aggregation.py
+++ object_oriented_python/aggregation.py
@@ -23,9 +23,7 @@ class DocType(Tag):
         super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
         self.end_tag = '' #doctype doesnt have an end tag
 
-
 class Head(Tag):
-
     def __init__(self, title=None):
         super().__init__('head', '')
         # self.contents was originally nothing, ie ''
