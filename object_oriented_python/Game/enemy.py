9142a156dcbaefeb20eadee2b2206709fd815b69
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-14 22:28:11 +0200

    Update enemy.py

--- object_oriented_python/Game/enemy.py
+++ object_oriented_python/Game/enemy.py
@@ -1,38 +1,37 @@
 import random
-# My go at the lives config, bit too much code
-# class Enemy:
-#
-#     def __init__(self, name="Enemy", hit_points=10, lives=1):
-#         self.name = name
-#         self.hit_points = hit_points
-#         self.lives = lives
-#         self.live_cap = hit_points
-#         self.alive = True
-#
-#     def take_damage(self, damage):
-#         remaining_points = self.hit_points - damage
-#         if self.alive == False:
-#             print(f"{self.name} is dead, cannot take more damage")
-#         elif remaining_points >= 0:
-#             self.hit_points = remaining_points
-#             print(f"I took {damage} points damage and have {self.hit_points} hp left")
-#         else:
-#             # if we're in here, we're losing at least one life
-#             lives_to_take = max(damage // self.live_cap,1)
-#             # Lock the lives to be 0 min
-#             self.lives = max(self.lives - lives_to_take, 0)
-#             # lock our hp to be 0. If we're here the remaining points is neg so cap + neg points works
-#             self.hit_points = max(self.live_cap + remaining_points, 0)
-#             print(f"{self.name} Lost {lives_to_take} live(s). remaining HP {max(self.hit_points,0)}")
-#             if self.lives == 0 & self.hit_points <=0:
-#                 print(f"{self.name} is DEAD")
-#                 self.alive = False
-#
-#     def __str__(self):
-#         return f"Name: {self.name}, " \
-#                f"lives: {self.lives}, " \
-#                f"Hit points: {self.hit_points}, " \
-#                f"Status: {['Alive' if self.alive ==True else 'Dead'][0]}"
+
+class Enemy:
+    def __init__(self, name="Enemy", hit_points=10, lives=1):
+        self.name = name
+        self.hit_points = hit_points
+        self.lives = lives
+        self.live_cap = hit_points
+        self.alive = True
+
+    def take_damage(self, damage):
+        remaining_points = self.hit_points - damage
+        if self.alive == False:
+            print(f"{self.name} is dead, cannot take more damage")
+        elif remaining_points >= 0:
+            self.hit_points = remaining_points
+            print(f"I took {damage} points damage and have {self.hit_points} hp left")
+        else:
+            # if we're in here, we're losing at least one life
+            lives_to_take = max(damage // self.live_cap,1)
+            # Lock the lives to be 0 min
+            self.lives = max(self.lives - lives_to_take, 0)
+            # lock our hp to be 0. If we're here the remaining points is neg so cap + neg points works
+            self.hit_points = max(self.live_cap + remaining_points, 0)
+            print(f"{self.name} Lost {lives_to_take} live(s). remaining HP {max(self.hit_points,0)}")
+            if self.lives == 0 & self.hit_points <=0:
+                print(f"{self.name} is DEAD")
+                self.alive = False
+
+    def __str__(self):
+        return f"Name: {self.name}, " \
+               f"lives: {self.lives}, " \
+               f"Hit points: {self.hit_points}, " \
+               f"Status: {['Alive' if self.alive ==True else 'Dead'][0]}"
 
 class Enemy:
 
