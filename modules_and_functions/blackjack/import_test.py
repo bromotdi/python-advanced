4093e8b7e901ff17021ae53f7d6b3663c3944dbd
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-16 23:52:36 +0200

    Update import_test.py

--- modules_and_functions/blackjack/import_test.py
+++ modules_and_functions/blackjack/import_test.py
@@ -1,6 +1,10 @@
 import blackjack
-# If do the below, things starting with _ wont be imported, python knows the convention of using _ to make them private
 from blackjack import *
+# If do the below, things starting with _ wont be imported, 
+# python knows the convention of using _ to make them private
+
+
+
 # Dont need to do .copy() when we do sorted, cos that also takes a copy
 g = sorted(globals())  #.copy()
 for x in g:
