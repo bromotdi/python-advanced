5a490f76466db4d8948ad915dfd8438f722c21e4
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-19 22:18:08 +0200

    Update challenge2-challenges.py

--- generators_and_comprehensions/challenge2-challenges.py
+++ generators_and_comprehensions/challenge2-challenges.py
@@ -17,7 +17,8 @@
 
 import timeit
  
-setup = '''\
+setup = '''
+
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
              2: "You are at the top of a hill",
@@ -32,6 +33,7 @@ exits = {0: {"Q": 0},
          4: {"N": 1, "W": 2, "Q": 0},
          5: {"W": 2, "S": 1, "Q": 0}}
 '''
+
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
              2: "You are at the top of a hill",
