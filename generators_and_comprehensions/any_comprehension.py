ab06bb01ffee5fa663b56c182ef53876551f9d11
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-19 22:14:04 +0200

    Update any_comprehension.py

--- generators_and_comprehensions/any_comprehension.py
+++ generators_and_comprehensions/any_comprehension.py
@@ -1,14 +1,11 @@
 from data import people, basic_plants_list, plants_list, plants_dict
 
-
 # writing an email and choose whos getting the email from contacts email
-
 # Check everyone has an email
 if all([person[1] for person in people]):
     print("Sending email")
 else:
     print("User must edit list of receipts")
-
 
 # check at least one planttype in a group is a grass
 # use named tuple here for easy access to plant.plant_type field
@@ -16,7 +13,6 @@ if any([plant.plant_type == "Grass" for plant in plants_list]):
     print("This plant contains grass")
 else:
     print("No grasses in this pack")
-
 
 # Challenge - use any & comprehension to check plant in plants dict to see if theres grasses there
 print("-"*80)
