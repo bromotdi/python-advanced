efc95e3402aef7c7d17a98aaa2486329ff04e631
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-19 22:18:27 +0200

    Update challenge2-challenges.py

--- generators_and_comprehensions/challenge2-challenges.py
+++ generators_and_comprehensions/challenge2-challenges.py
@@ -16,8 +16,6 @@
 # In other words, use a for loop to run the comprehension for each of the keys in the locations dictionary.
 
 import timeit
- 
-setup = '''
 
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
@@ -25,21 +23,7 @@ locations = {0: "You are sitting in front of a computer learning Python",
              3: "You are inside a building, a well house for a small stream",
              4: "You are in a valley beside a stream",
              5: "You are in the forest"}
-             
-exits = {0: {"Q": 0},
-         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         2: {"N": 5, "Q": 0},
-         3: {"W": 1, "Q": 0},
-         4: {"N": 1, "W": 2, "Q": 0},
-         5: {"W": 2, "S": 1, "Q": 0}}
-'''
 
-locations = {0: "You are sitting in front of a computer learning Python",
-             1: "You are standing at the end of a road before a small brick building",
-             2: "You are at the top of a hill",
-             3: "You are inside a building, a well house for a small stream",
-             4: "You are in a valley beside a stream",
-             5: "You are in the forest"}
 exits = {0: {"Q": 0},
          1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
          2: {"N": 5, "Q": 0},
