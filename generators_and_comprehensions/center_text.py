63e150ac95a3f8f29312b3ec50bf703a6ac08bd8
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-19 22:15:28 +0200

    Update center_text.py

--- generators_and_comprehensions/center_text.py
+++ generators_and_comprehensions/center_text.py
@@ -1,7 +1,4 @@
 def centre_text(*args):
-    # text = ""
-    # for arg in args:
-    #     text += str(arg) + " "
     text = "-".join([str(arg) for arg in args])
     left_margin = (80 - len(text)) // 2
     print(" " * left_margin, text)
