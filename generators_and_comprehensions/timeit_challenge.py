8bed446cbfbbb896373b94af363266a19bb18da1
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-24 22:43:35 +0200

    Update timeit_challenge.py

--- generators_and_comprehensions/timeit_challenge.py
+++ generators_and_comprehensions/timeit_challenge.py
@@ -1,8 +1,6 @@
-# In the section on Functions, we looked at 2 different ways to calculate the factorial
-# of a number.  We used an iterative approach, and also used a recursive function.
-#
+# In the section on Functions, we looked at 2 different ways to calculate the factorial of a number.  
+# We used an iterative approach, and also used a recursive function.
 # This challenge is to use the timeit module to see which performs better.
-#
 # The two functions appear below.
 #
 # Hint: change the number of iterations to 1,000 or 10,000.  The default
