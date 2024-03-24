11debafd1297125f2a9e03dc52205656bbe9454c
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-24 22:39:00 +0200

    Update named_tuples.py

--- generators_and_comprehensions/named_tuples.py
+++ generators_and_comprehensions/named_tuples.py
@@ -1,8 +1,8 @@
 from data import basic_plants_list, plants_list, plants_dict
 
 print(plants_list[0])
-# for plant in basic_plants_list:
-#     print(plant[0])
+for plant in basic_plants_list:
+    print(plant[0])
 
 for plant in plants_list:
     print(plant.name)
