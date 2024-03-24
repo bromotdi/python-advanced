0bad6c6e0d6dc7cf8558a4bb38095373e194115b
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-24 22:39:34 +0200

    Update nested_challenge.py

--- generators_and_comprehensions/nested_challenge.py
+++ generators_and_comprehensions/nested_challenge.py
@@ -1,11 +1,10 @@
-# for i in range(1, 11):
-#     for j in range(1,11):
-#         print(i, i*j)
+for i in range(1, 11):
+    for j in range(1,11):
+        print(i, i*j)
 
 # write a nested comprehension
 for val in [(i,j) for i in range(1,11) for j in range(1,11)]:
     print(f"{i} x {j} = {i*j}")
-
 
 gen = ((i,j) for i in range(1,11) for j in range(1,11))
 
