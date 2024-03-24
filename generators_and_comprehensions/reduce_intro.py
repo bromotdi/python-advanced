d632cd3847d8887b1303a21437cebb3e713db93a
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-24 22:42:40 +0200

    Update reduce_intro.py

--- generators_and_comprehensions/reduce_intro.py
+++ generators_and_comprehensions/reduce_intro.py
@@ -4,7 +4,7 @@ import functools
 def calc_values(x, y):
     return x+y
 
-numbers = [2,3,5, 8, 13]
+numbers = [2, 3, 5, 8, 13]
 
 reduced_value = functools.reduce(calc_values, numbers)
 print(reduced_value)
