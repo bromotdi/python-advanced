2b503bfb1aa528d3505170f65448ce7f0fd262bb
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2024-03-24 22:40:56 +0200

    Update pi_generator.py

--- generators_and_comprehensions/pi_generator.py
+++ generators_and_comprehensions/pi_generator.py
@@ -9,19 +9,18 @@ odds = oddnumbers()
 for i in range(100):
    print(next(odds))
 
-# my attempt
-# def pi_series():
-#     denom = odd()
-#     next(denom)
-#     n = 0
-#     start = 4
-#     while True:
-#         val = 4*next(denom)**-1
-#         if n % 2 == 0:
-#             val *= -1
-#         n += 1
-#         start += val
-#         yield start
+def pi_series():
+    denom = odd()
+    next(denom)
+    n = 0
+    start = 4
+    while True:
+        val = 4*next(denom)**-1
+        if n % 2 == 0:
+            val *= -1
+        n += 1
+        start += val
+        yield start
 
 # his effort
 def pi_series():
